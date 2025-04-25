7cabefec24e9df8bf0b1914e86666d17d62f8aa6
Aleksandr Chilikov <chilikov.a@orionnet.ru>
2025-04-25 09:25:18 +0700

    Fix: п.1 - введена конфигурация 'settings.enable_jaeger', отвечающая за обязательность использования opentelemetry (jaeger)

--- movies_service/src/core/jaeger_init.py
+++ movies_service/src/core/jaeger_init.py
@@ -7,19 +7,21 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 from .config import settings
 
-trace.set_tracer_provider(
-    TracerProvider(
-        resource=Resource(
-            attributes={
-                SERVICE_NAME: settings.service_name,
-            },
+
+if settings.enable_jaeger:
+    trace.set_tracer_provider(
+        TracerProvider(
+            resource=Resource(
+                attributes={
+                    SERVICE_NAME: settings.service_name,
+                },
+            ),
         ),
-    ),
-)
-otlp_exporter = OTLPSpanExporter(endpoint=settings.jaeger_http_endpoint)
-span_processor = BatchSpanProcessor(span_exporter=otlp_exporter)
+    )
+    otlp_exporter = OTLPSpanExporter(endpoint=settings.jaeger_http_endpoint)
+    span_processor = BatchSpanProcessor(span_exporter=otlp_exporter)
 
-tracer_provider = trace.get_tracer_provider()
-tracer_provider.add_span_processor(span_processor)
+    tracer_provider = trace.get_tracer_provider()
+    tracer_provider.add_span_processor(span_processor)
 
-tracer = trace.get_tracer(__name__)
+    tracer = trace.get_tracer(__name__)
